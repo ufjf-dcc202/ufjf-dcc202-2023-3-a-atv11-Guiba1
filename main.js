3064f998632a607a453029d0957d59b739d612a5
Guilherme Tavares <tavares.guilherme@estudante.ufjf.br>
2023-11-29 20:55:40 -0300

    o arquivo main.js foi alterado

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 // main.js
-import { limparLista } from "./lista.js";
+import { adicionarNaLista, limparLista } from "./lista.js";
 import { getLista } from "./lista.js"
 // Passo 1 - Ache os elementos importantes
 const pEntrada = document.querySelector('#entrada');
-const olSaida = document.querySelector('#saida');
+const olSaida = document.querySelector('#itens');
 const brnAdicionar = document.querySelector('#adicionar');
 const btnLimpar = document.querySelector('#limpar');
 
-olSaida.innerHTML = "";
-atualizarItensdDeLista();
-limparLista();
+btnLimpar.addEventListener('click, limparElementosDaLista');
+btnAdicionar.addEventListener('click', criaElementoNaLista);
+
 atualizarItensdDeLista();
 
+function criaElementoNaLista() {
+    const texto = pEntrada.textContent;
+    adicionarNaLista(texto);
+    atualizarItensdDeLista();
+    pEntrada.textContent="";
+    pEntrada.focus();
+}
+    function limparElementosDaLista(){
+        limparLista();
+        atualizarItensdDeLista();
+    }
+
+
 function atualizarItensdDeLista(){
+    olSaida.innerHTML = "";
     const lista = getLista();
     for (let i=0; i < getLista.length; i++){
         const item = lista[i];
