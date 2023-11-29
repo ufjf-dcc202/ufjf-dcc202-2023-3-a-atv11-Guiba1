c4b5f1dd31bbefbe72287ada00d464a3f1663aa0
Guilherme Tavares <tavares.guilherme@estudante.ufjf.br>
2023-11-29 20:43:29 -0300

    o arquivo lista.js foi alterado

--- lista.js
+++ lista.js
@@ -4,4 +4,11 @@ export function getLista(){
     return lista;
 }
 
+export function limparLista(){
+    lista.splice(0,3);
+}
+
+export function adicionarNaLista(novoItem){
+    lista.push(novoItem);
+}
 //export {getLista};
